8170737882cf4d21538c72b015335bd24dfef4d3
kimnamsun <nsunny0908@naver.com>
2021-04-24 11:41:25 +0900

    theme 변경

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,41 +1,14 @@
-var normal = document.getElementById("nav-menu");
-var reverse = document.getElementById("nav-menu-left");
+hljs.initHighlightingOnLoad();
 
-var icon = normal !== null ? normal : reverse;
-
-// Toggle the "menu-open" % "menu-opn-left" classes
-function toggle() {
-	  var navRight = document.getElementById("nav");
-	  var navLeft = document.getElementById("nav-left");
-	  var nav = navRight !== null ? navRight : navLeft;
-
-	  var button = document.getElementById("menu");
-	  var site = document.getElementById("wrap");
-	  
-	  if (nav.className == "menu-open" || nav.className == "menu-open-left") {
-	  	  nav.className = "";
-	  	  button.className = "";
-	  	  site.className = "";
-	  } else if (reverse !== null) {
-	  	  nav.className += "menu-open-left";
-	  	  button.className += "btn-close";
-	  	  site.className += "fixed";
-	  } else {
-	  	  nav.className += "menu-open";
-	  	  button.className += "btn-close";
-	  	  site.className += "fixed";
-	    }
-	}
-
-// Ensures backward compatibility with IE old versions
-function menuClick() {
-	if (document.addEventListener && icon !== null) {
-		icon.addEventListener('click', toggle);
-	} else if (document.attachEvent && icon !== null) {
-		icon.attachEvent('onclick', toggle);
-	} else {
-		return;
-	}
-}
-
-menuClick();
+$(document).ready(function(){
+                  $("h2,h3,h4,h5,h6").each(function(i,item){
+                                           var tag = $(item).get(0).localName;
+                                           $(item).attr("id","dir"+i);
+                                           $("#category").append('<a class="new'+tag+'" href="#dir'+i+'">'+$(this).text()+'</a></br>');
+                                           $(".newh2").css("margin-left",25);
+                                           $(".newh3").css("margin-left",45);
+                                           $(".newh4").css("margin-left",65);
+                                           $(".newh5").css("margin-left",85);
+                                           $(".newh6").css("margin-left",105);
+                                           });
+                  });
